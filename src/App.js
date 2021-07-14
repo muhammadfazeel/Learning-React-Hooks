9bdea14a74ce8d66b98afedcfaf16f2eaf9ab514
muhammadfazeel <p.pakistan47@gmail.com>
2021-07-15 02:14:04 +0500

    Forms Handling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Expense from './components/Expenses/Expense';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   return (
     <div className="App">
+      <NewExpense />
       <Expense />
     </div>
   );
